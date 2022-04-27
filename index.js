719d701478b52eb4afc0e060a955ed7fab691e82
nolangh <nolanghiggins@gmail.com>
2022-04-27 08:58:12 -0600

    -a

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const dayjs = require("dayjs");
 const app = express();
 const port = 8080;
 
+app.use(express.static("express"));
+
 app.get("/time", (req, res) => {
 	let current = dayjs().format("MM/DD/YYYY"); // NOTE: must be all caps
 	res.send(`${current}`);
+});
+
+app.get("/Brownies", (req, res) => {
+	res.sendFile("Brownies.txt");
 });
 
 app.listen(port, () => {
